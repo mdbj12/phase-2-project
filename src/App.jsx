3ce0d458cd898c09dd25a048fc7dfeaab8ecc827
Michael Jeung <michaelj229@gmail.com>
2023-04-25 14:52:34 -0400

    removed login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import HomePage from './HomePage/HomePage';
 import Menu from './Menu/Menu';
 import NavBar from './NavBar/NavBar';
 import Cart from './InCart/Cart'
-import Login from './Login/Login';
 
 function App(){
 
@@ -34,7 +33,6 @@ function App(){
                     <Route path='/' element={<HomePage />} />
                     <Route path='Menu' element={<Menu items={items} updateCart={updateCart} />} />
                     <Route path='Cart' element={<Cart cartItems={cartItems} />} />
-                    <Route path='Login' element={<Login />} />
                 </Routes>
             </BrowserRouter>
         </div>
